ed3e1896912bdf31c5ef3ef408be55b797d4dd19
aidenhemmings <agjhemmings@gmail.com>
2024-05-02 17:30:01 +0200

    Comment

--- app/src/main/java/com/example/opsc_part2/MainActivity.kt
+++ app/src/main/java/com/example/opsc_part2/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
+        // Declaring variables
         var btnregister : Button = findViewById(R.id.btnRegister)
         var btngoogle : Button = findViewById(R.id.btnGoogleLogin)
 
