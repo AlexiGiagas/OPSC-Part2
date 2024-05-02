74d293b01e9cd6987c32468c101bc1e985e13491
Tristan Stavast <stavasttristan@gmail.com>
2024-05-02 19:03:38 +0200

    Created buttons for all views

--- app/src/main/java/com/example/opsc_part2/Home.kt
+++ app/src/main/java/com/example/opsc_part2/Home.kt
@@ -1,6 +1,8 @@
 package com.example.opsc_part2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,38 @@ class Home : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        /*Timesheet
+        Categories
+        ListofEntries
+        TotalHours*/
+        var timesheet : Button = findViewById(R.id.btnTimesheet)
+        var categories : Button = findViewById(R.id.btnCategories)
+        var listentries : Button = findViewById(R.id.btnListEntries)
+        var tothours : Button = findViewById(R.id.btnTotalHours)
+
+        timesheet.setOnClickListener()
+        {
+            val t = Intent(this, Timesheet::class.java)
+            startActivity(t)
+        }
+
+        categories.setOnClickListener()
+        {
+            val c = Intent(this, Categories::class.java)
+            startActivity(c)
+        }
+
+        listentries.setOnClickListener()
+        {
+            val le = Intent(this, ListofEntries::class.java)
+            startActivity(le)
+        }
+
+        tothours.setOnClickListener()
+        {
+            val th = Intent(this, TotalHours::class.java)
+            startActivity(th)
+        }
+
     }
 }
