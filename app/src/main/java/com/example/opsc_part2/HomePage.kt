74f0dc946bd090226b205bf6627202cd17aa1499
AlexiGiagas <alexi.giagas@gmail.com>
2024-05-02 18:07:50 +0200

    Update HomePage.kt

--- app/src/main/java/com/example/opsc_part2/HomePage.kt
+++ app/src/main/java/com/example/opsc_part2/HomePage.kt
@@ -8,7 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomePage : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
