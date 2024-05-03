66adb1d786090cb58fee35476080aba75abe9f4e
AlexiGiagas <alexi.giagas@gmail.com>
2024-05-03 16:51:18 +0200

    fixed minor errors

--- app/src/main/java/com/example/opsc_part2/MinMaxHours.kt
+++ app/src/main/java/com/example/opsc_part2/MinMaxHours.kt
@@ -1,13 +1,16 @@
 package com.example.opsc_part2
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MinMaxHours : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MinMaxHours : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_min_max_hours)
@@ -15,6 +18,13 @@ class MinMaxHours : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var btnaddminmax : Button = findViewById(R.id.btnAddMinMaxHours)
+
+        btnaddminmax.setOnClickListener()
+        {
+
         }
     }
 }
