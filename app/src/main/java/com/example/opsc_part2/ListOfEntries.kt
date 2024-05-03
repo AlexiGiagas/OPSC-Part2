63fc3e848ef92610f7a63db6711182cbff2d482e
aidenhemmings <agjhemmings@gmail.com>
2024-05-03 19:42:11 +0200

    updated

--- app/src/main/java/com/example/opsc_part2/ListOfEntries.kt
+++ app/src/main/java/com/example/opsc_part2/ListOfEntries.kt
@@ -24,36 +24,6 @@ class ListOfEntries : AppCompatActivity() {
             insets
         }
 
-        var btnSearch: Button = findViewById(R.id.btnSearchEntries)
-        btnSearch.setOnClickListener() {
-            val startdate: TextView = findViewById(R.id.txtStartDate)
-            val enddate: TextView = findViewById(R.id.txtEndDate)
-
-            val pattern = Pattern.compile("\\d{2}-\\d{2}-\\d{4}")
-            val matcher1 = pattern.matcher(startdate.text.toString())
-            val matcher2 = pattern.matcher(enddate.text.toString())
-
-            if ((matcher1.matches()) && (matcher2.matches())) {
-                val formatter = SimpleDateFormat("dd-MM-yyyy")
-                for (arg in Timesheet.arrTimesheet) {
-                    val date = formatter.format(arg.Date)
-
-                    val temp1 = formatter.parse(startdate.text.toString())
-                    val sdate = formatter.format(temp1)
-                    val temp2 = formatter.parse(enddate.text.toString())
-                    val edate = formatter.format(temp2)
-
-                    if ((date >= sdate) && (date <= edate)) {
-                        Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-
-
-        var back: Button = findViewById(R.id.btnBack)
-        back.setOnClickListener() {
-
             //Declaring bac button variable
             var back: Button = findViewById(R.id.btnBack)
 
@@ -90,4 +60,3 @@ class ListOfEntries : AppCompatActivity() {
 
         }
     }
-}
