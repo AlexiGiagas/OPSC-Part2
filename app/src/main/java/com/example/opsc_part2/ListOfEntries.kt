4792aad2e43a78b7a21f682ba81e76ce59e861b5
aidenhemmings <agjhemmings@gmail.com>
2024-05-03 19:52:34 +0200

    update

--- app/src/main/java/com/example/opsc_part2/ListOfEntries.kt
+++ app/src/main/java/com/example/opsc_part2/ListOfEntries.kt
@@ -24,42 +24,7 @@ class ListOfEntries : AppCompatActivity() {
             insets
         }
 
-            //Declaring bac button variable
-            var back: Button = findViewById(R.id.btnBack)
 
-            //Setting an on click event to go to the home page
-            back.setOnClickListener()
-            {
-
-                val intent = Intent(this, Home::class.java)
-                startActivity(intent)
-            }
-
-            var search : Button = findViewById(R.id.btnSearchEntries)
-            search.setOnClickListener()
-            {
-                //Declaring variables
-                val startdate: TextView = findViewById(R.id.txtStartDate)
-                val enddate: TextView = findViewById(R.id.txtEndDate)
-                val pattern = Pattern.compile("\\d{2}-\\d{2}-\\d{4}")
-                val matcher1 = pattern.matcher(startdate.text.toString())
-                val matcher2 = pattern.matcher(enddate.text.toString())
-
-                if ((matcher1.matches()) && (matcher2.matches())) {
-                    val formatter = SimpleDateFormat("dd-MM-yyyy")
-                    for (arg in Timesheet.arrTimesheet) {
-                        val date = formatter.format(arg.Date)
-                        val temp1 = formatter.parse(startdate.text.toString())
-                        val sdate = formatter.format(temp1)
-                        val temp2 = formatter.parse(enddate.text.toString())
-                        val edate = formatter.format(temp2)
-
-                        if ((date >= sdate) && (date <= edate)) {
-                            Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-            }
 
         }
     }
