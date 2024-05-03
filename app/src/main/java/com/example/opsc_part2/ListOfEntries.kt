0320c30eb716ff5f4a3f20be85d71cdcf20922c9
aidenhemmings <agjhemmings@gmail.com>
2024-05-03 19:27:29 +0200

    worked on listofentries

--- app/src/main/java/com/example/opsc_part2/ListOfEntries.kt
+++ app/src/main/java/com/example/opsc_part2/ListOfEntries.kt
@@ -3,10 +3,15 @@ package com.example.opsc_part2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.regex.Pattern
 
 class ListOfEntries : AppCompatActivity()
 {
@@ -27,5 +32,32 @@ class ListOfEntries : AppCompatActivity()
             val intent = Intent(this, Home::class.java)
             startActivity(intent)
         }
+
+        val startdate : TextView = findViewById(R.id.txtStartDate)
+        val enddate : TextView = findViewById(R.id.txtEndDate)
+
+        val pattern = Pattern.compile("\\d{2}-\\d{2}-\\d{4}")
+        val matcher1 = pattern.matcher(startdate.text.toString())
+        val matcher2 = pattern.matcher(enddate.text.toString())
+
+        if ((matcher1.matches()) && (matcher2.matches()))
+        {
+            val formatter = SimpleDateFormat("dd-MM-yyyy")
+            for (arg in Timesheet.arrTimesheet)
+            {
+                val date = formatter.format(arg.Date)
+
+                val temp1 = formatter.parse(startdate.text.toString())
+                val sdate = formatter.format(temp1)
+                val temp2 = formatter.parse(enddate.text.toString())
+                val edate = formatter.format(temp2)
+
+                if ((date >= sdate) && (date <= edate))
+                {
+                    Toast.makeText(this, "Yes", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 }
