9616455e32aa132d05d2c774b7dd231f89f987b2
Tristan Stavast <stavasttristan@gmail.com>
2024-05-03 12:23:34 +0200

    Added TimesheetData class to store timesheet data
    Aiden just double check

---
+++ app/src/main/java/com/example/opsc_part2/TimesheetData.kt
@@ -0,0 +1,13 @@
+package com.example.opsc_part2
+
+import java.sql.Time
+import java.util.Date
+
+data class TimesheetData (
+    val date : Date,
+    val StartTime : Time,
+    val EndTime : Time,
+    val Description : String,
+    val imageURL : String
+)
+//Double check if this is fine!!
