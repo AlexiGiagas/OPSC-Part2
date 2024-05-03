1323f11676df8c4a8e2859f60fe33740fe7c406f
aidenhemmings <agjhemmings@gmail.com>
2024-05-03 16:55:32 +0200

    worked on categories

--- app/src/main/java/com/example/opsc_part2/Categories.kt
+++ app/src/main/java/com/example/opsc_part2/Categories.kt
@@ -1,8 +1,10 @@
 package com.example.opsc_part2
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -26,6 +28,10 @@ class Categories : AppCompatActivity()
         }
 
         var btnaddcategory : Button = findViewById(R.id.btnAddCategory)
+        var listbox : ListView = findViewById(R.id.listBox)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrCategories)
+
+        listbox.adapter = adapter
 
         btnaddcategory.setOnClickListener()
         {
@@ -39,8 +45,10 @@ class Categories : AppCompatActivity()
             {
                 arrCategories.add(CategoriesData(categ.text.toString()))
                 Toast.makeText(this, "Successfully added Category.", Toast.LENGTH_SHORT).show()
+                adapter.notifyDataSetChanged()
             }
         }
 
     }
+
 }
